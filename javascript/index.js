ed868bb143e02ebd037a4b5cdd8581a41152a5ff
bushrahDev <yakubikimot29@gmail.com>
2025-06-15 01:24:44 -0700

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,31 @@
+console.log('hello world');
+
+// const
+// const name=''
+'opeyemi'
+"ola"
+
+// ===** string **===
+
+const school ='al-ameen'
+
+
+// ===** number **===
+const age = 21
+
+// ===** boolen**===
+const isComoplete = true;
+const isGood = false;
+
+
+
+
+
+const middleName = 'ayinla'
+console.log(middleName)
+
+const day = 1
+console.log(day)
+
+const isBad = false
+console.log(isBad)
