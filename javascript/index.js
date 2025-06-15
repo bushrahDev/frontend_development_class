de32a3e95e5a426c4baa32eba1f89c64a9d992c9
bushrahDev <yakubikimot29@gmail.com>
2025-06-15 04:08:00 -0700

    added variable

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,29 @@
+
+
 console.log('hello world');
+
+// there are three keywords to define a variable
+
+/*
+1: const 
+2: let
+3: var
+*/
+
+
+// string
+const name = 'sulaiman';
+
+// number
+const level = 300;
+
+
+// boolean
+// ===** true or false **===
+
+const isExpensive = true;
+const isGood = false;
+
 
 // const
 // const name=''
@@ -9,23 +34,106 @@ console.log('hello world');
 
 const school ='al-ameen'
 
+console.log(school)
 
 // ===** number **===
 const age = 21
-
+console.log(age)
 // ===** boolen**===
-const isComoplete = true;
-const isGood = false;
+const isComplete = true;
+const isQuality = false;
+
+const year = null;
+
+const classRoom = undefined;
+const friend = 'zainab'
+console.log(friend)
+
+const fridge = {
+    name:'soft plus',
+    color:'white',
+    height:56,
+    switch:true,
+    year:null,
+    brand:undefined
+}
+// console.log(fridge)
+console.log(fridge.name)
+// console.log(fridge.year)
+console.log(fridge['year'])
+
+console.log(fridge['brand'])
+
+// const middleName = 'ayinla'
+// console.log(middleName)
+
+// const day = 1
+// console.log(day)
+
+// const isBad = false
+// console.log(isBad)
 
 
 
 
 
-const middleName = 'ayinla'
-console.log(middleName)
 
-const day = 1
-console.log(day)
 
-const isBad = false
-console.log(isBad)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     const container = document.querySelector('#box')
+
+
+// const fetchData = async()=>{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await res.json()
+//     console.log(data)
+//     container.innerHTML = data.map(user=>{
+//         return `<div>
+//         <h1>name:${user.name}</h1>
+//         <p>email:${user.email}</p>
+//         </div>
+//         `
+//     }).join(' ')
+// }
+
+// fetchData()
---
+++ javascript/variable.js
@@ -0,0 +1,44 @@
+
+
+// ==** The general rules for constructing names for variables (unique identifiers) are: **==
+
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter.
+// Names can also begin with $ and _ (but we will not use it in this tutorial).
+// Names are case sensitive (y and Y are different variables).
+// Reserved words (like JavaScript keywords) cannot be used as names.
+
+// format of defining a variable
+// 1: camel case
+// 2: pascal case
+// 3: snake case
+
+/**
+ * camel case
+ */
+
+const name = 'fatai'
+
+const firstName = 'sulaiman'
+ 
+const lastName = 'al_ameen'
+
+const onDataPush = true;
+
+/**
+ * pascal case
+ */
+
+const SchoolName = 'al-ameen';
+const MiddleName = 'sulaiman';
+
+/**
+ * snake case
+ */
+const friend_name = 'yusuf';
+const my_age = 78;
+
+const y = 89;
+console.log(y)
+
+const $class = 'ss1'
