110c7109238a515c9c0de4e5b2d3456093939eb0
ismailDev02 <ismailolatunde355@gmail.com>
2025-06-18 10:34:02 -0700

    added arrays

---
+++ javascript/_array/array.js
@@ -0,0 +1,28 @@
+const people = [
+    {
+        name:'ayomide',
+        email: 'olatundeismail355@gmail.com',
+        year: 2005,
+        isGood:true,
+    },
+    {
+        name:'ayomide',
+        email:'olatundeismail355@gmail.com',
+        year:2005,
+        isGood:true,
+    },
+    {
+        name:'ayomide',
+        email:'olatundeismail355@gmail.com',
+        year:2005,
+        isGood:true,
+    }
+
+]
+// console.log(people[1])
+// console.log(people[1].year)
+console.log(people)
+
+const years = [1,2,3,67,89,56,78]
+console.log(years[2])
+
